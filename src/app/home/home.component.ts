9fc9aa8386e561c2f601db7abf947c40699b3b99
mjbf0748 <mjbf0748@gmail.com>
2020-08-09 23:27:42 -0400

    updating UI

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { UploadService } from './../../../services/upload.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  selectedFiles: FileList;
+  progressInfos = [];
+  message = '';
+  data;
+
+  fileInfos: Observable<any>;
+
+  constructor(private uploadService: UploadService) { }
+
+
+  ngOnInit(): void {
+    // this.fileInfos = this.uploadService.getFiles();
+  }
+
+
+  selectFiles(event): void {
+    this.progressInfos = [];
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadFiles(): void {
+    this.message = '';
+    // console.log(this.selectedFiles)
+    this.upload(this.selectedFiles);
+
+  }
+
+  upload(file): void {
+
+    this.uploadService.upload(file).subscribe(
+      event => {
+        if (event.type === HttpEventType.UploadProgress) {
+
+        } else if (event instanceof HttpResponse) {
+          this.data = event.body;
+          console.log(this.data)
+          let source = JSON.stringify(event.body);
+
+
+          // this.fileInfos = this.uploadService.getFiles();
+        }
+
+      },
+      err => {
+        this.message = 'Could not upload the file:' + file.name;
+      });
+  }
+
+}
